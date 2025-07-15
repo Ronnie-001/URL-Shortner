3b1b111ef3b69eeade257c69895d18ff838ced72
Ronnie-001 <ronald.adesida@gmail.com>
2025-07-15 17:27:46 +0100

    add: main endpoints

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root() -> str:
-    return "Hello, world!"
+    return "A URL Shortner!"
+
+
+@app.get("/api/v1/urls/shorten")
+async def shortenURL():
+    print()
+
+@app.get("/api/v1/urls/{url_id}")
+async def getURL():
+    print()
+
+
